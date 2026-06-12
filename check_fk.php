72462a48c5fdd7518d9d8b46d2648ce039cefcc8
Muhammad Anurohim <muhammadanurohim23@gmail.com>
2026-06-12 17:06:40 +0700

    Remove accidental check_fk.php script

--- check_fk.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("BASEPATH", __DIR__ . "/system/");
-define("APPPATH", __DIR__ . "/application/");
-define("ENVIRONMENT", "development");
-require_once BASEPATH . "core/CodeIgniter.php";
-
